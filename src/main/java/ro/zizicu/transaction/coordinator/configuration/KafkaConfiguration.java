4484167d772c0cc509183faa7d63e3c2e681e84f
Matei Florescu <matei@yahoo.com>
2022-12-08 22:16:44 +0200

    Removed Kafka support

--- src/main/java/ro/zizicu/transaction/coordinator/configuration/KafkaConfiguration.java
+++ src/main/java/ro/zizicu/transaction/coordinator/configuration/KafkaConfiguration.java
@@ -1,57 +1,57 @@
-package ro.zizicu.transaction.coordinator.configuration;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaAdmin;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import ro.zizicu.nwbase.transaction.TransactionMessage;
-
-@Configuration
-public class KafkaConfiguration {
-
-    private final Environment environment;
-
-    public KafkaConfiguration(Environment environment) {
-        this.environment = environment;
-    }
-
-    @Bean
-    public KafkaAdmin admin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("kafka.url"));
-        return new KafkaAdmin(configs);
-    }
-
-    @Bean
-    public ProducerFactory<String, TransactionMessage> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(producerConfigs());
-    }
-
-    @Bean
-    public Map<String, Object> producerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("kafka.url"));
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return props;
-    }
-
-    @Bean
-    public KafkaTemplate<String, TransactionMessage> kafkaTemplate()
-    {
-        return new KafkaTemplate<String, TransactionMessage>(producerFactory());
-    }
-
-
-}
+//package ro.zizicu.transaction.coordinator.configuration;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import org.apache.kafka.clients.admin.AdminClientConfig;
+//import org.apache.kafka.clients.producer.ProducerConfig;
+//import org.apache.kafka.common.serialization.StringSerializer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.env.Environment;
+//import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+//import org.springframework.kafka.core.KafkaAdmin;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.kafka.core.ProducerFactory;
+//import org.springframework.kafka.support.serializer.JsonSerializer;
+//
+//import ro.zizicu.nwbase.transaction.TransactionMessage;
+//
+//@Configuration
+//public class KafkaConfiguration {
+//
+//    private final Environment environment;
+//
+//    public KafkaConfiguration(Environment environment) {
+//        this.environment = environment;
+//    }
+//
+//    @Bean
+//    public KafkaAdmin admin() {
+//        Map<String, Object> configs = new HashMap<>();
+//        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("kafka.url"));
+//        return new KafkaAdmin(configs);
+//    }
+//
+//    @Bean
+//    public ProducerFactory<String, TransactionMessage> producerFactory() {
+//        return new DefaultKafkaProducerFactory<>(producerConfigs());
+//    }
+//
+//    @Bean
+//    public Map<String, Object> producerConfigs() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("kafka.url"));
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+//        return props;
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, TransactionMessage> kafkaTemplate()
+//    {
+//        return new KafkaTemplate<String, TransactionMessage>(producerFactory());
+//    }
+//
+//
+//}
--- src/main/java/ro/zizicu/transaction/coordinator/controllers/RestExceptionHandler.java
+++ src/main/java/ro/zizicu/transaction/coordinator/controllers/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package ro.zizicu.transaction.coordinator.controllers;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,16 +8,20 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import lombok.extern.slf4j.Slf4j;
 import ro.zizicu.transaction.coordinator.exceptions.DistributedTransactionNotFound;
 import ro.zizicu.transaction.coordinator.exceptions.RestError;
 
 @ControllerAdvice
+@Slf4j
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = { DistributedTransactionNotFound.class })
+    @ExceptionHandler(value = {DistributedTransactionNotFound.class, DataAccessException.class })
     protected ResponseEntity<Object> handleConflict(RuntimeException exception, WebRequest webRequest) {
+    	log.error(exception.getMessage());
         return handleExceptionInternal(exception,
-                new RestError("Distributed transaction not found", 1),
+                new RestError(exception.getMessage(), 1),
                 new HttpHeaders(),
                 HttpStatus.BAD_REQUEST,
                 webRequest);
